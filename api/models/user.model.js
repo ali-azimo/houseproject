f9eed0a1fb6d715e7a66eb68a59cc17d587f1e8c
Ali Azimo <webdesign.aejl@gmail.com>
2023-10-16 22:32:44 +0200

    Criado usuarios com modelo sucesso

---
+++ api/models/user.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+const userShema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+},
+    { timestamps: true });
+const User = mongoose.Model('User', userShema);
+export default User;
